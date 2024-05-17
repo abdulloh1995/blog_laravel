180b2309fe514ae46e6b1d1daae2a9ac7e6a1d77
Abdulloh <developer.sobirov4448@gmail.com>
2024-05-17 15:20:39 +0500

    blog

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::group(['namespace' => 'Main'], function () {
+    Route::get('/', 'IndexController');
+});
+
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController');
+    });
+});
+
+Auth::routes();
+
