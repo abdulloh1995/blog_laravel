0e6421c61215d278af5e9909422bb0f8d26679d5
Abdulloh <developer.sobirov4448@gmail.com>
2024-05-17 16:05:18 +0500

    category page

--- app/Http/Controllers/Admin/Category/IndexController.php
+++ app/Http/Controllers/Admin/Category/IndexController.php
@@ -1,14 +1,13 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Main;
+namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return view('admin.index');
+        return view('admin.categories.index');
     }
 }
---
+++ resources/views/admin/include/sidebar.blade.php
@@ -0,0 +1,31 @@
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="image">
+                    <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+                </div>
+                <div class="info">
+                    <a href="#" class="d-block">Alexander Pierce</a>
+                </div>
+            </div>
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-header">MAIN</li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-th-list"></i>
+                        <p>
+                            Category
+                        </p>
+                    </a>
+                </li>
+
+            </ul>
+
+        </div>
+        <!-- /.sidebar -->
+    </aside>
